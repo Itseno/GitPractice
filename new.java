38ab2c47fa3fde101bfcbf78ef7d0737bf1cd75a
Ian Crossey <crossei@stlprioryschool.org>
2020-01-29 17:56:27 -0600

    new.java

---
+++ new.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Random;
+class RoboThree {
+  public static void main(String[] args) {
+    
+    int compChoice = getRandomNumberInRange(1, 3);
+    
+    System.out.println(compChoice);
+    
+  }
+  private static int getRandomNumberInRange(int min, int max) {
+
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
+	}
+  
+}
+
